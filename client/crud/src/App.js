8f84cb7ca927034bf3120cd7b535f42b0a342da9
AthilaMatheusBorges <athila.borges@ccc.ufcg.edu.br>
2022-02-09 12:12:06 -0300

    primeiro envio

---
+++ client/crud/src/App.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios'
+import './App.css'
+import Card from './components/cards/card'
+
+function App() {
+  const [values, setValues] = useState()
+  const [listGames, setListGames] = useState()
+
+  const handleChangeValues = value => {
+    //console.log(value.target.value);
+    setValues(prevValue => ({
+      ...prevValue,
+      [value.target.name]: value.target.value
+    }))
+  }
+
+  useEffect(() => {
+    Axios.get('http://localhost:3001/getCards').then(response => {
+      setListGames(response.data)
+    })
+  })
+
+  const handleClickButton = () => {
+    Axios.post('http://localhost:3001/register', {
+      name: values.name,
+      cost: values.cost,
+      category: values.category
+    }).then(res => {
+      console.log(res)
+    })
+  }
+
+  return (
+    <div className="app--container">
+      <div className="register--container">
+        <h1 className="register--title">Game Shop</h1>
+        <input
+          type="text"
+          name="name"
+          placeholder="Nome"
+          className="register--input"
+          onChange={handleChangeValues}
+        />
+        <input
+          type="text"
+          name="cost"
+          placeholder="Preço"
+          className="register--input"
+          onChange={handleChangeValues}
+        />
+        <input
+          type="text"
+          name="category"
+          placeholder="Categoria"
+          className="register--input"
+          onChange={handleChangeValues}
+        />
+        <button
+          className="register--button"
+          onClick={() => handleClickButton()}
+        >
+          Cadastrar
+        </button>
+      </div>
+      {typeof listGames !== 'undefined' &&
+        listGames.map(value => {
+          return (
+            <Card
+              key={value.idgames}
+              listCard={listGames}
+              setListCard={setListGames}
+              id={value.idgames}
+              name={value.name}
+              cost={value.cost}
+              category={value.category}
+            ></Card>
+          )
+        })}
+      <Card></Card>
+    </div>
+  )
+}
+
+export default App
